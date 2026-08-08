ca2261228a02eb46a388c16e8273ccecfe66bc05
Amrith P <amrith@Amriths-MacBook-Air.local>
2026-08-08 20:32:43 +0530

    ui modifications

--- UI/src/pages/projects/ProjectDetailsPage.tsx
+++ UI/src/pages/projects/ProjectDetailsPage.tsx
@@ -245,6 +245,7 @@ export const ProjectDetailsPage: React.FC = () => {
       ) : (
         <div className="bg-white rounded-xl shadow-sm border border-gray-100 p-6 overflow-hidden">
           <Board />
+          
         </div>
       )}
 
